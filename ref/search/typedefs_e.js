b6cf7c359c55a22afa9546e2806757b304585337
meeq <meeq@users.noreply.github.com>
2026-06-08 18:27:24 +0000

    deploy: 2cbe782ca6e05c64b27b0060378580bc474f7ed1

--- ref/search/typedefs_e.js
+++ ref/search/typedefs_e.js
@@ -1,6 +1,8 @@
 var searchData=
 [
   ['sample_5fptr_5ft_0',['sample_ptr_t',['../samplebuffer_8h.html#aef020c0b192c534ea79e1f94c8c7228f',1,'samplebuffer.h']]],
-  ['subrenderer_5ft_1',['subrenderer_t',['../subtitles_8h.html#a8221ca3ec3fbe555a09a6d4d0668645b',1,'subtitles.h']]],
-  ['syscall_5fhandler_5ft_2',['syscall_handler_t',['../exception_8h.html#aceadbbfdaee48a1075dac31bddc20d1c',1,'exception.h']]]
+  ['sprite_5fdecodable_5ffn_1',['sprite_decodable_fn',['../sprite__internal_8h.html#a1b88083350e959bffb6a64696268b04f',1,'sprite_internal.h']]],
+  ['sprite_5fdecode_5ffn_2',['sprite_decode_fn',['../sprite__internal_8h.html#a5475a99feebf99b9ea3460a09e9433a9',1,'sprite_internal.h']]],
+  ['subrenderer_5ft_3',['subrenderer_t',['../subtitles_8h.html#a8221ca3ec3fbe555a09a6d4d0668645b',1,'subtitles.h']]],
+  ['syscall_5fhandler_5ft_4',['syscall_handler_t',['../exception_8h.html#aceadbbfdaee48a1075dac31bddc20d1c',1,'exception.h']]]
 ];
